8be500eb587dc22b5de4cc7e13bb6f367af956b6
BluePr1nt-X <71233060+BluePr1nt-X@users.noreply.github.com>
2021-01-10 01:18:06 -0600

    Create Post.sh

---
+++ Post.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+# UFW Firewall Configs
+sudo ufw default deny outgoing
+sudo ufw default deny incoming
+sudo ufw allow out 53
+sudo ufw allow out http
+sudo ufw allow out https
+sudo ufw enable
+sudo service ufw restart
+
+
+#Load Dock/Remove Dir
+sudo rm ~/.config/plank/dock1/launchers/*
+sudo cp -a /usr/LockBox/. ~/.config/plank/dock1/launchers
+rm -rf /usr/LockBox/
+
+#Adjust Wallpaper
+sudo gsettings set org.gnome.desktop.background picture-uri "file:// ~usr/share/backgrounds/LOCKBOX WALLPAPER-01.jpg"
