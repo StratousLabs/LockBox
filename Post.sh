508bfac4842db39a645b92c8c67350fe349fdc81
BluePr1nt-X <71233060+BluePr1nt-X@users.noreply.github.com>
2021-01-24 00:58:39 -0600

    Update Post.sh
    Cleanup

--- Post.sh
+++ Post.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 
-
-
-#Load Dock/Remove Dir
-sudo rm ~/.config/plank/dock1/launchers/*
-sudo cp -a /usr/Lockbox-Plank/. ~/.config/plank/dock1/launchers
-
-sudo apt update -y && sudo apt upgrade -y
-
-
 # UFW Firewall Configs
-sudo ufw default deny outgoing
-sudo ufw default deny incoming
-sudo ufw allow out 53
-sudo ufw allow out http
-sudo ufw allow out https
 sudo ufw enable
 sudo service ufw restart
 
 
-#Clean Logs/Folders
-sudo chmod a+rwx ~/.config/plank/dock1/launchers
-#find /var/log -type f -delete
-sudo rm -rf /usr/Lockbox-Plank/
-rm *.dockitem
-sudo rm -rf /usr/Post.sh
+
 
